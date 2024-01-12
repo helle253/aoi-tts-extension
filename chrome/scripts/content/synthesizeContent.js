1a8e9dc2c716ea5379eca4d4926e60bf84c06177
Nathan-Heller <nathan.heller@bluebottlecoffee.com>
2024-01-12 13:19:38 -0600

    add log statement

--- chrome/scripts/content/synthesizeContent.js
+++ chrome/scripts/content/synthesizeContent.js
@@ -1,6 +1,7 @@
 async function synthesizeContent() {
   const text =  window.getSelection().toString();
   // Send message to offscreen document
+  console.log('starting request in content script', text);
   await chrome.runtime.sendMessage({
     type: 'init-synthesize',
     text,
